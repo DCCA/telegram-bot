00d147b263dedc7bce9f2e702797c0b69b3cd489
dcca <dcca12@gmail.com>
2020-05-09 19:39:46 -0300

    add fix

--- controllers/ticker.js
+++ controllers/ticker.js
@@ -3,26 +3,50 @@ const TELEGRAM_API = process.env.TELEGRAM_API;
 const fetch = require('node-fetch');
 const numeral = require('numeral');
 
-exports.getTicker = async (req, res, next) => {
+exports.getTicker = (req, res, next) => {
 	console.log(req.body);
-	// Change this currency to get from chat bot
+	// Change this currency to get from chatbot
 	const currencyId = req.body.message.text;
 	const chatId = req.body.message.chat.id;
+	let currencyName;
+	let formattedPrice;
 	// Build the URI
 	const uri = `https://api.nomics.com/v1/currencies/ticker?key=${NOMICS_API}&ids=${currencyId}&interval=1d`;
 	// Get response
-	try {
-		const rawData = await fetch(uri, {
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		});
-		const data = rawData.json();
-		const currencyName = data[0].name;
-		const formattedPrice = numeral(data[0].price).format('$ 0.0000');
-		const telegramResult = await fetch(
-			`https://api.telegram.org/bot${TELEGRAM_API}/sendMessage`,
-			{
+	fetch(uri, {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	})
+		.then((result) => {
+			return result.json();
+		})
+		.then((data) => {
+			if (data.length < 1) {
+				fetch(`https://api.telegram.org/bot${TELEGRAM_API}/sendMessage`, {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify({
+						from: 'CryptoBot',
+						text: `No data found`,
+						chat_id: chatId,
+					}),
+				})
+					.then((result) => {
+						console.log(result);
+					})
+					.catch((err) => {
+						console.log(err);
+					});
+				return res.status(200).json(true);
+			}
+			currencyName = data[0].name;
+			formattedPrice = numeral(data[0].price).format('$ 0.0000');
+		})
+		.then((result) => {
+			fetch(`https://api.telegram.org/bot${TELEGRAM_API}/sendMessage`, {
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json',
@@ -32,25 +56,16 @@ exports.getTicker = async (req, res, next) => {
 					text: `${currencyName}: ${formattedPrice}`,
 					chat_id: chatId,
 				}),
-			}
-		);
-		console.log(telegramResult);
-		return res.status(200).json(true);
-	} catch {
-		const telegramResult = await fetch(
-			`https://api.telegram.org/bot${TELEGRAM_API}/sendMessage`,
-			{
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					from: 'CryptoBot',
-					text: `No currency found`,
-					chat_id: chatId,
-				}),
-			}
-		);
-		return res.status(200).json(true);
-	}
+			})
+				.then((result) => {
+					console.log(result);
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+			return res.status(200).json(true);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 };

2e016351ef4cb9f685282a222fe1adee202e338e
dcca <dcca12@gmail.com>
2020-05-09 19:03:40 -0300

    fix body

--- controllers/ticker.js
+++ controllers/ticker.js
@@ -3,38 +3,53 @@ const TELEGRAM_API = process.env.TELEGRAM_API;
 const fetch = require('node-fetch');
 const numeral = require('numeral');
 
-exports.getTicker = async (req, res, next) => {
+exports.getTicker = (req, res, next) => {
 	console.log(req.body);
 	// Change this currency to get from chatbot
 	const currencyId = req.body.message.text;
 	const chatId = req.body.chat.id;
+	let currencyName;
+	let formattedPrice;
 	// Build the URI
 	const uri = `https://api.nomics.com/v1/currencies/ticker?key=${NOMICS_API}&ids=${currencyId}&interval=1d`;
 	// Get response
-	const dataRaw = await fetch(uri, {
+	fetch(uri, {
 		headers: {
 			'Content-Type': 'application/json',
 		},
-	});
-	// Transform to JSON
-	const data = await dataRaw.json();
-	if (!data) {
-		const err = new Error('No data found');
-		err.statusCode = 404;
-		throw err;
-	}
-	console.log(data);
-	const currencyName = data[0].name;
-	const formattedPrice = await numeral(data[0].price).format('$ 0.0000');
-	await fetch(`https://api.telegram.org/bot${TELEGRAM_API}/sendMessage`, {
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body: JSON.parse({
-			from: 'CryptoBot',
-			text: `${currencyName}: ${formattedPrice}`,
-			chat_id: chatId,
-		}),
-	});
-	return res.status(200).json(true);
+	})
+		.then((res) => {
+			return res.json();
+		})
+		.then((data) => {
+			if (!data) {
+				const err = new Error('No data found');
+				err.statusCode = 404;
+				throw err;
+			}
+			currencyName = data[0].name;
+			formattedPrice = numeral(data[0].price).format('$ 0.0000');
+		})
+		.then((res) => {
+			fetch(`https://api.telegram.org/bot${TELEGRAM_API}/sendMessage`, {
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					from: 'CryptoBot',
+					text: `${currencyName}: ${formattedPrice}`,
+					chat_id: chatId,
+				}),
+			})
+				.then((res) => {
+					console.log(res);
+					return res.status(200).json(true);
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 };

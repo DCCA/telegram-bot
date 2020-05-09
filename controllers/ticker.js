c8c5bda788e704487d406ad5cedaf40f39f212b0
dcca <dcca12@gmail.com>
2020-05-09 18:50:50 -0300

    add response

--- controllers/ticker.js
+++ controllers/ticker.js
@@ -1,4 +1,5 @@
 const NOMICS_API = process.env.NOMICS_API;
+const TELEGRAM_API = process.env.TELEGRAM_API;
 const fetch = require('node-fetch');
 const numeral = require('numeral');
 
@@ -6,6 +7,7 @@ exports.getTicker = async (req, res, next) => {
 	console.log(req.body);
 	// Change this currency to get from chatbot
 	const currencyId = req.body.message.text;
+	const chatId = req.body.chat.id;
 	// Build the URI
 	const uri = `https://api.nomics.com/v1/currencies/ticker?key=${NOMICS_API}&ids=${currencyId}&interval=1d`;
 	// Get response
@@ -24,5 +26,15 @@ exports.getTicker = async (req, res, next) => {
 	console.log(data);
 	const currencyName = data[0].name;
 	const formattedPrice = await numeral(data[0].price).format('$ 0.0000');
+	await fetch(`https://api.telegram.org/bot${TELEGRAM_API}/sendMessage`, {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.parse({
+			from: 'CryptoBot',
+			text: `${currencyName}: ${formattedPrice}`,
+			chat_id: chatId,
+		}),
+	});
 	return res.status(200).json(true);
 };
